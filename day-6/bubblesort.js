523ed48da4e4dd20382e2f736fff8d7651c3253b
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-04-20 13:06:35 +0530

    day-6

---
+++ day-6/bubblesort.js
@@ -0,0 +1,17 @@
+const arr=[1,4,1,4,6,87,0,3,2,5,2];
+
+
+function bubbleSort(arr){
+for(let i=0;i<arr.length;i++){
+  for(let j=1;j<arr.length;j++){
+  if(arr[j]<arr[j-1]){
+    const temp = arr[j];
+    arr[j] = arr[j - 1];
+    arr[j - 1] = temp;
+  }
+  }
+
+}}
+
+bubbleSort(arr);
+console.log(arr);
