9e949af2fb55862574a15f2aa551338d170c955e
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-05-13 22:20:56 +0530

    day-6

---
+++ day-6/bubbleSort1.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+}
+const arr = [1, 5, 2, 6, 4, 7];
+console.log(bubbleSort(arr));
+console.log(arr);
--- day-6/bubblesort.js
+++ day-6/bubblesort.js
@@ -3,7 +3,7 @@ const arr=[1,4,1,4,6,87,0,3,2,5,2];
 
 function bubbleSort(arr){
 for(let i=0;i<arr.length;i++){
-  for(let j=1;j<arr.length;j++){
+  for(let j=0;j<arr.length;j++){
   if(arr[j]<arr[j-1]){
     const temp = arr[j];
     arr[j] = arr[j - 1];
